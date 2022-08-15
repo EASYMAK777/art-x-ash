5a2236be2f79e1a81ea9ffa0c763f81ccb55e41e
JuanFlores <juanflz7777@gmail.com>
2022-08-15 12:28:23 -0700

    styling images

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,25 +3,40 @@ import amy3 from "../img/Amy3.jpg";
 import amy1 from "../img/amy1.jpg";
 import amy2 from "../img/Amy2.jpg";
 
-function IndividualIntervalsExample() {
+function ImageCarousel() {
   return (
     <Carousel className="carousel">
-      <Carousel.Item interval={1000}>
-        <img className="art-work" src={amy3} alt="picasso" controls="false" />
+      <Carousel.Item className="carousel-item" interval={1000}>
+        <img
+          className="art-work"
+          src={amy3}
+          alt="picasso"
+          style={{ height: 500, width: 500 }}
+        />
         {/* <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption> */}
       </Carousel.Item>
-      <Carousel.Item interval={1000}>
-        <img className="art-work" src={amy1} alt="Second slide" />
+      <Carousel.Item className="carousel-item" interval={1000}>
+        <img
+          className="art-work"
+          src={amy1}
+          alt="Second slide"
+          style={{ height: 500, width: 500 }}
+        />
         {/* <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption> */}
       </Carousel.Item>
-      <Carousel.Item interval={1000}>
-        <img className="art-work" src={amy2} alt="Third slide" />
+      <Carousel.Item className="carousel-item" interval={1000}>
+        <img
+          className="art-work"
+          src={amy2}
+          alt="Third slide"
+          style={{ height: 500, width: 500 }}
+        />
         {/* <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
@@ -33,4 +48,4 @@ function IndividualIntervalsExample() {
   );
 }
 
-export default IndividualIntervalsExample;
+export default ImageCarousel;
